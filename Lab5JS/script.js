73563575af843675273cedcd6e90ba85f81a21ef
OscarEscriba <128449179+OscarEscriba@users.noreply.github.com>
2024-02-22 17:49:06 -0600

    Lab5 commit

---
+++ Lab5JS/script.js
@@ -0,0 +1,30 @@
+function enviarMensaje() {
+    // Obtén el valor del textarea por su ID
+    var mensajeTextArea = document.getElementById("mensaje-chat");
+    // Obtén el valor del textarea sin necesidad de .value aquí
+    var mensaje = mensajeTextArea.value.trim();
+
+    if (mensaje.length > 0) {
+        // Crea un nuevo elemento div para representar el mensaje
+        var nuevoMensaje = document.createElement("div");
+        nuevoMensaje.className = "chat";
+        nuevoMensaje.textContent = mensaje;
+
+        // Agrega el nuevo mensaje al contenedor de mensajes
+        var contenedorMensajes = document.getElementById("mensaje");
+        contenedorMensajes.appendChild(nuevoMensaje);
+
+        // Limpia el área de texto después de enviar el mensaje
+        mensajeTextArea.value = "";
+    }
+}
+
+function enviarConEnter(event) {
+    if (event.key === "Enter") {
+        // Evita que se realice un salto de línea en el área de texto
+        event.preventDefault();
+        // Llama a la función enviarMensaje al presionar Enter
+        enviarMensaje();
+    }
+}
+
