f5e4e88121c7a679b8b5b43032bc59b349df6e3c
OscarEscriba <128449179+OscarEscriba@users.noreply.github.com>
2024-05-27 13:44:12 -0600

    animacion del inicio

--- miportafolio/src/Views/Header.js
+++ miportafolio/src/Views/Header.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Scrollspy from 'react-scrollspy';
 import './Header.css';
 
 const Header = () => {
+  useEffect(() => {
+    const handleHomeClick = () => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
+    document.getElementById('home-link').addEventListener('click', handleHomeClick);
+
+    return () => {
+      document.getElementById('home-link').removeEventListener('click', handleHomeClick);
+    };
+  }, []);
+
   return (
     <header>
       <h1>Mi Portafolio</h1>
       <nav>
-        <ul>
-          <li><a href='#Home'>Home</a></li>
+        <Scrollspy items={['home', 'about', 'projects', 'tecnologias', 'Contact']} currentClassName="is-current" activeClass="active" offset={-50}>
+          <li><a id="home-link" href="#home">Home</a></li>
           <li><a href="#about">Sobre mí</a></li>
           <li><a href="#projects">Proyectos</a></li>
-          <li><a href="#Tecnologias">Tecnologias</a></li>
-          <li><a href="#contact">Contacto</a></li>
-        </ul>
+          <li><a href="#tecnologias">Tecnologías</a></li>
+          <li><a href="#Contact">Contacto</a></li>
+        </Scrollspy>
       </nav>
     </header>
   );
