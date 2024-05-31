a8afb751a2694975519e5e1a1109a82219095228
OscarEscriba <128449179+OscarEscriba@users.noreply.github.com>
2024-05-30 21:03:01 -0600

    Ultimo commit portafolio

--- miportafolio/src/Views/Historial.js
+++ miportafolio/src/Views/Historial.js
@@ -3,10 +3,11 @@ import React from 'react';
 import styles from './Historial.module.css';
 
 const events = [
-  { date: '2020-01-01', title: 'Evento 1', description: 'Descripción del evento 1' },
-  { date: '2020-06-01', title: 'Evento 2', description: 'Descripción del evento 2' },
-  { date: '2021-01-01', title: 'Evento 3', description: 'Descripción del evento 3' },
-  { date: '2021-06-01', title: 'Evento 4', description: 'Descripción del evento 4' },
+  { date: 'Octubre, 2020', title: 'Me gradué del Colegio', description: 'me gradué de bachiller en ciencias y letras con Orientación en computación. Desde este momento sabía que la programación iba a ser parte importante de mi vida' },
+  { date: 'Enero 2021', title: 'Ingresé a UVG', description: 'Inicialmente ingresé a Ingeniería mecatrónica, pero poco a poco me fui dando cuenta de mi verdadera pasión' },
+  { date: 'Noviembre, 2022', title: 'Primer curso de Programación', description: 'Como parte de la curiosidad que me despertaba la programación, decidí realizar mi primer curso independiente de JavaScript. En donde tuve una introducción a los frameworks de desarrollo y al desarrollo web' },
+  { date: 'Julio, 2023', title: 'Inicio la ingenieria en Ciencias de la computación', description: 'Logré tomar la decisión de cambiarme a ingeniería en ciencias de la computación, terminando de confirmar lo que más me gustaba hacer, que era programar.' },
+  { date: 'Diciembre, 2023', title: 'Me involucro en mi primer proyecto profesional', description: 'Gracias al cambio mencionado anteriormente, me involucré en mi primer proyecto profesional. Dicho proyecto  me ayudó a saber que parte del desarrollo web me gusta más y a aprender nuevas tecnologías.' },
   // Agrega más eventos según sea necesario
 ];
 
--- miportafolio/src/Views/Projects.js
+++ miportafolio/src/Views/Projects.js
@@ -6,17 +6,19 @@ import blog from './blog.jpg';
 import calculadora from './calculadora.jpg';
 import clinica from './Clinica.jpg';
 import chat from './Chat.jpg';
+import HistorialHTML from './HistorialHTML.jpg';
+import radar from './radar.jpg';
 
 const projects = [
   {
     id: 1,
     name: 'Primer trabajo en CSS',
     technology: 'CSS',
-    description: ( 
+    description: (
       <p>
-        'Este trabajo me permitió desarrollar mis habilidades con CSS <br/>
-         lo pongo dentro de mi portafolio, ya que lo considero de suma importancia en el inicio del proceso de <br/>
-         aprendizaje de dicha tecnología'
+        Este trabajo me permitió desarrollar mis habilidades con CSS <br/>
+        lo pongo dentro de mi portafolio, ya que lo considero de suma importancia en el inicio del proceso de <br/>
+        aprendizaje de dicha tecnología.
       </p>
     ),
     image: css,
@@ -33,7 +35,7 @@ const projects = [
         <br/> 
         Este fue creado con react para el frontend y Firebase para <br/> 
         el backend, utilizando FireStore como base de datos y las functions <br/> 
-        para crear una especie de API para realizar consultas en la misma <br/> 
+        para crear una especie de API para realizar consultas en la misma. <br/> 
       </p>
     ),
     image: blog,
@@ -45,14 +47,14 @@ const projects = [
     description: (
       <p>
         Este fue el primer trabajo como profesional en el que he estado <br/> 
-        directamente involucrado<br/> 
-        El mismo consta de una aplicacion web que ayudara para la administracion<br/> 
-        de una clinica medica.<br/> 
+        directamente involucrado. <br/> 
+        El mismo consta de una aplicacion web que ayudara para la administracion <br/> 
+        de una clinica medica. <br/> 
         <br/> 
-        Este proyecto tuvo una curva de aprendizaje grande, debido a no tener conocimiento<br/> 
-        acerca de vue.<br/> 
-        Al final pudimos implementar de una optima manera firestore de firebase y se planea seguir<br/> 
-        implementando servicios de google.<br/> 
+        Este proyecto tuvo una curva de aprendizaje grande, debido a no tener conocimiento <br/> 
+        acerca de vue. <br/> 
+        Al final pudimos implementar de una optima manera firestore de firebase y se planea seguir <br/> 
+        implementando servicios de google. <br/> 
       </p>
     ),
     image: clinica,
@@ -63,12 +65,12 @@ const projects = [
     technology: 'React y test',
     description: (
       <p>
-        Este trabajo nos sirvio para indagar en el mundo de<br/> 
-        los test, los cuales son de mucha ayuda para darle manetenimiento<br/> 
-        a nuestro codigo y garantizar su continuidad a lo largo de los años <br/> 
+        Este trabajo nos sirvio para indagar en el mundo de <br/> 
+        los test, los cuales son de mucha ayuda para darle manetenimiento <br/> 
+        a nuestro codigo y garantizar su continuidad a lo largo de los años. <br/> 
         <br/> 
         También nos ayudó a desarrollar y seguir practicando las habilidades <br/> 
-        lógicas para trabajar en javascript<br/> 
+        lógicas para trabajar en javascript. <br/> 
       </p>
     ),
     image: calculadora,
@@ -79,17 +81,46 @@ const projects = [
     technology: 'JS y APIRest',
     description: (
       <p>
-        Este fue el primer proyecto en el que utilzamos e <br/> 
+        Este fue el primer proyecto en el que utilizamos e <br/> 
         implementamos lo que fue un API, con el mismo <br/> 
         nos enseñaron poco a poco en como crear una por nuestro <br/> 
-        lado y como realizar consultas CRUD a la misma<br/> 
-        Así mismo, desarrollamos habilidades en JS y css<br/> 
+        lado y como realizar consultas CRUD a la misma. <br/> 
+        Así mismo, desarrollamos habilidades en JS y css. <br/> 
       </p>
     ), 
-    image:chat,
+    image: chat,
+  },
+  {
+    id: 6,
+    name: 'Primer trabajo HTML',
+    technology: 'HTML',
+    description: (
+      <p>
+        En este proyecto trabajamos todas las etiquetas y herramientas que<br/>
+        nos ofrece html para poder crear paginas web.<br/>
+        Este proyecto me gusto porque fue de las primeras cosas en las <br/>
+        que ponia especial atencion a la parte del html y me doy cuenta <br/>
+        como puedo organizar de mejor manera las cosas.<br/>
+      </p>
+    ),
+    image:HistorialHTML, // Cambia esto con la ruta de la imagen correcta
+  },
+  {
+    id: 7,
+    name: 'Radar',
+    technology: 'React y firebase',
+    description: (
+      <p>
+        Este proyeco consistio en hacer un radar que mostrara la ubicacion de<br/>
+        las demas personas en relacion con la mia <br/>
+        Para ello utilizamos google Functions para mi api <br/>
+        y firestore en mi base de datos, para que la aplicacion<br/>
+        al momento de logearnos, nos permitiera dar nuestra ubicacion y mostrarla<br/>
+      </p>
+    ),
+    image: radar, // Cambia esto con la ruta de la imagen correcta
   }
 ];
-
 const Projects = () => {
   const [selectedProject, setSelectedProject] = useState(null);
 
@@ -103,10 +134,10 @@ const Projects = () => {
 
   return (
     <div className={styles.projectsContainer} id='projects'>
-<h2 style={{ textAlign: 'center', fontSize: '2.5em', fontFamily: 'cursive' }}>Proyectos</h2>
+      <h2 style={{ textAlign: 'center', fontSize: '2.5em', fontFamily: 'cursive' }}>Proyectos y Laboratorios</h2>
       <div className={styles.gridContainer}>
         <div className={styles.column}>
-          {projects.slice(0, 2).map((project) => (
+          {projects.slice(0, Math.ceil(projects.length / 3)).map((project) => (
             <div
               key={project.id}
               className={`${styles.projectItem} ${selectedProject === project.id ? styles.selected : ''}`}
@@ -120,19 +151,21 @@ const Projects = () => {
           ))}
         </div>
         <div className={`${styles.column} ${styles.middleColumn}`}>
-          <div
-            key={projects[2].id}
-            className={`${styles.projectItem} ${selectedProject === projects[2].id ? styles.selected : ''}`}
-            onClick={() => handleProjectClick(projects[2].id)}
-          >
-            <img src={projects[2].image} alt={projects[2].name} />
-            <div className={styles.projectOverlay}>
-              <h3>{projects[2].name}</h3>
+          {projects.slice(Math.ceil(projects.length / 3), Math.ceil((2 * projects.length) / 3)).map((project) => (
+            <div
+              key={project.id}
+              className={`${styles.projectItem} ${selectedProject === project.id ? styles.selected : ''}`}
+              onClick={() => handleProjectClick(project.id)}
+            >
+              <img src={project.image} alt={project.name} />
+              <div className={styles.projectOverlay}>
+                <h3>{project.name}</h3>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
         <div className={styles.column}>
-          {projects.slice(3).map((project) => (
+          {projects.slice(Math.ceil((2 * projects.length) / 3)).map((project) => (
             <div
               key={project.id}
               className={`${styles.projectItem} ${selectedProject === project.id ? styles.selected : ''}`}
@@ -154,7 +187,7 @@ const Projects = () => {
             <p>Tecnología utilizada: {projects.find((project) => project.id === selectedProject).technology}</p>
             <p>{projects.find((project) => project.id === selectedProject).description}</p>
             <img src={projects.find((project) => project.id === selectedProject).image} alt="Project"
-             style={{ maxWidth: '100%', maxHeight: '300px' }} />
+              style={{ maxWidth: '100%', maxHeight: '300px' }} />
           </div>
         </div>
       )}
